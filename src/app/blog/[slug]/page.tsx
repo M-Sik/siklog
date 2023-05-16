5ba4de6359208421677c5a2db10c844a21659cb1
김명식 <rlaaudtlr233@naver.com>
2023-05-16 20:30:41 +0900

    [feat] 게시글 상세페이지 동적 meta tag 구현

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,15 +9,15 @@ interface IProps {
 }
 // 다이나믹한 메타데이터
 // slug에 따라 메타데이터를 바꾸고 싶을때에는 generateMetadata 사용
-// export async function generateMetadata({ params: { slug } }: IProps) {
-//   // 포스트 정보 조회 api
-//   const { title, description } = await getPostData(slug);
-//   return {
-//     title: title,
-//     description: description,
-//     keywords: '제품 블로그',
-//   };
-// }
+export async function generateMetadata({ params: { slug } }: IProps) {
+  // 포스트 정보 조회 api
+  const { title, subtitle, keywords } = await getPostDetail(slug);
+  return {
+    title: title,
+    description: subtitle,
+    keywords: keywords,
+  };
+}
 
 export default async function BlogPage({ params: { slug } }: IProps) {
   console.log('포스트 상세정보 아이디 => ', slug);
