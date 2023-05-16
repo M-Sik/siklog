4b89c014ee37ee364e81287d7a90cc0140c734d5
김명식 <rlaaudtlr233@naver.com>
2023-05-16 21:36:30 +0900

    [feat] 사이트맵 구현 -> app/sitemap.ts - route 들의 사이트맵 구현 - home 화면에서 최근 5개 게시글의 동적 사이트맵 구현

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { recentPosts } from '@/service/post';
+import { MetadataRoute } from 'next';
+
+const URL = 'http://localhost:3000';
+
+export default async function sitemap() {
+  const posts = await recentPosts();
+
+  const routesMap = ['', '/blog'].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const postsMap = posts.map(({ _id }) => ({
+    url: `${URL}/blog/${_id}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routesMap, ...postsMap];
+}
