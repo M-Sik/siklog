5cc9ca78110bb5b24f516b702229e5a464722ebd
김명식 <rlaaudtlr233@naver.com>
2023-05-17 17:33:32 +0900

    [design] main recent post list 호버시 백그라운드 부여

--- src/components/cards/PostListCard.tsx
+++ src/components/cards/PostListCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function PostListCard({ post: { _id, createdAt, title, subtitle } }: Props) {
   return (
-    <div className="py-6 group">
+    <div className="py-6 md:px-4 group hover:bg-gray-100 rounded-lg">
       <Link href={`/blog/${_id}`} aria-label="블로그 상세보기 링크">
         <div>
           <p className=" text-sm text-gray-500">{createdAt}</p>
