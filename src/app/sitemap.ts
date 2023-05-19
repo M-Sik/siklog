6afce20f0625f62f4b0c12151ecdcef56846fb25
김명식 <rlaaudtlr233@naver.com>
2023-05-20 03:00:00 +0900

    [feat] 블로그 페이지 사이트맵 작성

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 
 export default async function sitemap() {
   const posts = await getRecentPosts();
-  const routesMap = ['', '/blog'].map((route) => ({
+  const routesMap = ['', '/blog', '/blog/sitemap.xml'].map((route) => ({
     url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}${route}`,
     lastModified: new Date().toISOString(),
   }));
