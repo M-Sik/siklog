07fba9dfda53ef1b595f8d29d6bfc139a5a61ca6
김명식 <rlaaudtlr233@naver.com>
2023-05-25 21:52:15 +0900

    [modify] 카테고리 페이지 카테고리 칩 모바일 사이즈에서 flex wrap으로 수정 및 호버시 백그라운드 추가

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -13,11 +13,11 @@ export default function CategoryPage() {
   return (
     <section>
       <h2 className="font-bold text-4xl">📚 Category</h2>
-      <div className="my-12 flex gap-6">
+      <div className="my-12 flex gap-3 md:gap-6 flex-wrap text-sm md:text-base">
         {categorys.map(({ title, category }) => (
           <div
             key={title}
-            className={`border-2 border-yellow-400 px-3 py-2 rounded-xl cursor-pointer ${
+            className={`border-2 border-yellow-400 px-2 md:px-3 py-1 md:py-2 rounded-xl cursor-pointer md:hover:bg-yellow-200 ${
               category === selectCategory && 'bg-yellow-400 text-white'
             }`}
             onClick={() => handleCategory(category)}
