796b1e1e8c2961b4170995e1927697f48a884eda
김명식 <rlaaudtlr233@naver.com>
2023-05-25 21:37:34 +0900

    [feat] 카테고리 페이지 생성 및 카테고리 선택 가능한 칩 구현

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+'use client';
+
+import React, { MouseEvent, useState } from 'react';
+import { categorys } from '@/data/categorys';
 
 export default function CategoryPage() {
-  return <div>CategoryPage</div>;
+  const [selectCategory, setSelectCategory] = useState('');
+
+  const handleCategory = (updateCategory: string) => {
+    setSelectCategory(updateCategory);
+  };
+
+  return (
+    <section>
+      <h2 className="font-bold text-4xl">📚 Category</h2>
+      <div className="my-12 flex gap-6">
+        {categorys.map(({ title, category }) => (
+          <div
+            key={title}
+            className={`border-2 border-yellow-400 px-3 py-2 rounded-xl cursor-pointer ${
+              category === selectCategory && 'bg-yellow-400 text-white'
+            }`}
+            onClick={() => handleCategory(category)}
+          >
+            {title}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
