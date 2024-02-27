bba14e07ff4ed27ef20601a6eeddcfa8bf2f8c25
김명식 <rlaaudtlr233@naver.com>
2024-02-27 17:37:39 +0900

    [fix] 배포중 포스트 api 결과값을 사이트맵에 등록할경우 오류가 발생하여 임시방편 사이트맵 수정

--- src/app/blog/sitemap.ts
+++ src/app/blog/sitemap.ts
@@ -2,11 +2,31 @@ import { getAllPosts } from '@/service/post';
 import { MetadataRoute } from 'next';
 
 export default async function sitemap() {
-  const allPosts = await getAllPosts();
+  // const allPosts = await getAllPosts();
 
-  const allPostsMap = allPosts.map(({ _id }) => ({
-    url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}/blog/${_id}`,
-    lastModified: new Date().toISOString(),
-  }));
-  return [...allPostsMap];
+  // const allPostsMap = allPosts.map(({ _id }) => ({
+  //   url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}/blog/${_id}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
+  // return [...allPostsMap];
+  return [
+    {
+      url: 'https://acme.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://acme.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://acme.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,14 +2,34 @@ import { getRecentPosts } from '@/service/post';
 import { MetadataRoute } from 'next';
 
 export default async function sitemap() {
-  const posts = await getRecentPosts();
-  const routesMap = ['', '/blog', '/blog/sitemap.xml'].map((route) => ({
-    url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}${route}`,
-    lastModified: new Date().toISOString(),
-  }));
-  const postsMap = posts.map(({ _id }) => ({
-    url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}/blog/${_id}`,
-    lastModified: new Date().toISOString(),
-  }));
-  return [...routesMap, ...postsMap];
+  // const posts = await getRecentPosts();
+  // const routesMap = ['', '/blog', '/blog/sitemap.xml'].map((route) => ({
+  //   url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}${route}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
+  // const postsMap = posts.map(({ _id }) => ({
+  //   url: `${process.env.NEXT_PUBLIC_CLIENT_API_URL}/blog/${_id}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
+  // return [...routesMap, ...postsMap];
+  return [
+    {
+      url: 'https://acme.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://acme.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://acme.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
 }
