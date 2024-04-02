8b6bf991d3255cb873a964c0bbdc23da5ed53e55
김명식 <rlaaudtlr233@naver.com>
2024-04-02 13:28:43 +0900

    [refactor] ProfileCard 컴포넌트 시맨틱태그 적용

--- src/components/cards/ProfileCard.tsx
+++ src/components/cards/ProfileCard.tsx
@@ -4,6 +4,11 @@ import ProfileImg from '../../../public/images/home/siklog_profile.jpeg';
 import GitHubIcon from '../icons/GitHubIcon';
 import TistoryIcon from '../icons/TistoryIcon';
 
+const links = [
+  { url: 'https://github.com/m-sik', label: '깃허브 링크', icon: <GitHubIcon /> },
+  { url: 'https://sikk.tistory.com/', label: '티스토리 링크', icon: <TistoryIcon /> },
+];
+
 export default function ProfileCard() {
   return (
     <section className="flex flex-col md:flex-row gap-6 justify-center items-center my-12">
@@ -23,14 +28,17 @@ export default function ProfileCard() {
           만렙이 없는 프로그래밍이라는 게임을 즐기며 <br />
           높은 레벨을 달성하기 위해 열심히 레벨업 하고있습니다.
         </p>
-        <div className="flex gap-4 items-center mt-2">
-          <a href={'https://github.com/m-sik'} target="_blank" aria-label="깃허브 링크">
-            <GitHubIcon />
-          </a>
-          <a href={'https://sikk.tistory.com/'} target="_blank" aria-label="티스토리 링크">
-            <TistoryIcon />
-          </a>
-        </div>
+        <nav>
+          <ul className="flex gap-4 items-center mt-2">
+            {links.map(({ url, label, icon }, i) => (
+              <li key={i}>
+                <a href={url} target="_blank" aria-label={label}>
+                  {icon}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </article>
     </section>
   );
