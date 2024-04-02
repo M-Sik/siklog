4221e285d9995a4ea1890d6632fa13fbf3191e71
김명식 <rlaaudtlr233@naver.com>
2024-04-02 13:23:46 +0900

    [refactor] 풋터 시맨틱 태그 적용

--- src/components/footers/Footer.tsx
+++ src/components/footers/Footer.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import GitHubIcon from '../icons/GitHubIcon';
 import TistoryIcon from '../icons/TistoryIcon';
 
+const links = [
+  { url: 'https://github.com/m-sik', label: '깃허브 링크', icon: <GitHubIcon /> },
+  { url: 'https://sikk.tistory.com/', label: '티스토리 링크', icon: <TistoryIcon /> },
+];
+
 export default function Footer() {
   return (
     <footer className="flex justify-center border-t py-6">
       <div>
-        <div className="flex justify-center items-center gap-6">
-          <a href={'https://github.com/m-sik'} target="_blank" aria-label="깃허브 링크">
-            <GitHubIcon />
-          </a>
-          <a href={'https://sikk.tistory.com/'} target="_blank" aria-label="티스토리 링크">
-            <TistoryIcon />
-          </a>
-        </div>
+        <nav>
+          <ul className="flex justify-center items-center gap-6">
+            {links.map(({ url, label, icon }, i) => (
+              <li key={i}>
+                <a href={url} target="_blank" aria-label={label}>
+                  {icon}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
         <p className="mt-2">Copyright 2023. MyeongSik Kim</p>
         <p className="text-center mt-2">dev.sik</p>
       </div>
