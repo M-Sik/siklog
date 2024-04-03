3af6892ae152e100903a0d09cdcdc858a8d71edf
김명식 <rlaaudtlr233@naver.com>
2024-04-03 12:45:17 +0900

    [refactor] 게시글 카드 컴포넌트에 날자 표시태그 p -> time으로 변경

--- src/components/cards/PostListCard.tsx
+++ src/components/cards/PostListCard.tsx
@@ -11,7 +11,7 @@ export default function PostListCard({ post: { _id, createdAt, title, subtitle }
     <div className="py-6 md:px-4 group hover:bg-gray-100 rounded-lg">
       <Link href={`/blog/${_id}`} aria-label="블로그 상세보기 링크">
         <div>
-          <p className=" text-sm text-gray-500">{createdAt}</p>
+          <time className=" text-sm text-gray-500">{createdAt}</time>
           <dl>
             <dt className="font-bold text-2xl mt-2 group-hover:text-yellow-400 transition-all duration-500">
               {title}
