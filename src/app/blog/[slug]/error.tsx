bef7cff9558391446f68f2f258c939b67de099c8
김명식 <rlaaudtlr233@naver.com>
2024-04-29 17:54:19 +0900

    [feat] 게시글 상세조회 페이지에 에러 핸들링을 위해 error.tsx 파일 추가

---
+++ src/app/blog/[slug]/error.tsx
@@ -0,0 +1,28 @@
+'use client'; // Error components must be Client Components
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2 className="text-xl font-bold text-center">
+        게시글을 불러오는 중 오류가 발생했습니다! 아래 버튼을 통해 재시도를 해보세요!
+      </h2>
+      <div className="flex justify-center mt-8">
+        <button className="bg-yellow-400 text-white py-3 px-4 rounded-lg" onClick={() => reset()}>
+          다시 시도하기!
+        </button>
+      </div>
+    </div>
+  );
+}
