a64e33d9177368308a3cf28fe32ab368fb6a503f
김명식 <rlaaudtlr233@naver.com>
2024-04-29 17:29:11 +0900

    [feat] 메인페이지에 로딩시 스켈레톤톤ui 구현

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+import MainLoading from '@/components/loadings/MainLoading';
+import React from 'react';
+
+export default function loading() {
+  return <MainLoading />;
+}
---
+++ src/components/loadings/MainLoading.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function MainLoading() {
+  return (
+    <div className="w-full animate-pulse">
+      <div className="px-6 pb-4">
+        <div className="w-[80%] h-10 mt-2 bg-slate-200 rounded-xl"></div>
+      </div>
+      <div className="px-6 py-6 flex items-center gap-6">
+        <div className=" w-72 h-64 bg-slate-200 rounded-xl"></div>
+        <div className="flex-1">
+          <div className="h-6 bg-slate-200 rounded-xl"></div>
+          <div className="w-[80%] h-6 mt-2 bg-slate-200 rounded-xl"></div>
+          <div className="w-full h-5 mt-1 bg-slate-200 rounded-xl"></div>
+        </div>
+      </div>
+      <div className="px-6 py-4">
+        <div className=" w-20 h-3 bg-slate-200 rounded-xl"></div>
+        <div className="w-[80%] h-6 mt-2 bg-slate-200 rounded-xl"></div>
+        <div className="w-full h-5 mt-1 bg-slate-200 rounded-xl"></div>
+      </div>
+      <div className="px-6 py-4">
+        <div className=" w-20 h-3 bg-slate-200 rounded-xl"></div>
+        <div className="w-[80%] h-6 mt-2 bg-slate-200 rounded-xl"></div>
+        <div className="w-full h-5 mt-1 bg-slate-200 rounded-xl"></div>
+      </div>
+      <div className="px-6 py-4">
+        <div className=" w-20 h-3 bg-slate-200 rounded-xl"></div>
+        <div className="w-[80%] h-6 mt-2 bg-slate-200 rounded-xl"></div>
+        <div className="w-full h-5 mt-1 bg-slate-200 rounded-xl"></div>
+      </div>
+      <div className="px-6 py-4">
+        <div className=" w-20 h-3 bg-slate-200 rounded-xl"></div>
+        <div className="w-[80%] h-6 mt-2 bg-slate-200 rounded-xl"></div>
+        <div className="w-full h-5 mt-1 bg-slate-200 rounded-xl"></div>
+      </div>
+      <div className="px-6 py-4">
+        <div className=" w-20 h-3 bg-slate-200 rounded-xl"></div>
+        <div className="w-full h-6 mt-2 bg-slate-200 rounded-xl"></div>
+        <div className="w-full h-5 mt-1 bg-slate-200 rounded-xl"></div>
+      </div>
+    </div>
+  );
+}
